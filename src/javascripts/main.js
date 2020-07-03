d5c80bc14e29521dd12e2be5e29b5eeeac74b79a
Brooke Nemchak <bnemchak@gmail.com>
2020-07-03 18:07:04 -0500

    step one req met

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,24 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+import myNavBar from './components/myNavBar/myNavBar';
+import authData from './helpers/data/authData';
+
 import '../styles/main.scss';
 
-console.error('Here');
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
+  auth.loginButton();
+  myNavBar.logOutEvent();
+
+  // $('body').on('mouseenter', '.myco-card', (e) => {
+  //   e.target.closest('.card').classList.add('bg-dark');
+  // });
+  // $('body').on('mouseleave', '.myco-card', (e) => {
+  //   e.target.closest('.card').classList.remove('bg-dark');
+  // });
+};
+
+init();

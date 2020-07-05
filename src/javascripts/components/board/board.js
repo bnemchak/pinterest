2873c7838714de40c1e0c09e024b0f8547c4b35c
Brooke Nemchak <bnemchak@gmail.com>
2020-07-05 13:12:06 -0500

    board h1 finally prints

--- src/javascripts/components/board/board.js
+++ src/javascripts/components/board/board.js
@@ -3,11 +3,14 @@ import './board.scss';
 const boardMaker = (board) => {
   const domString = `
   <div class="row">
-      <div class="card boardCard" id="${board.id}">
+    <div class ="col-3">
+      <div class="card" id="${board.id}">
         <div class="card-body">
           <h5 class="card-title">${board.category}</h5>
         </div>
+      </div>
     </div>
+  </div>
   `;
 
   return domString;
